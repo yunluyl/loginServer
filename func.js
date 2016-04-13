66ab6830888cd78e3e5372178ba8faab6773b19e
Yun <luyun198993@gmail.com>
2016-04-13 15:58:53 +0000

    test

--- func.js
+++ func.js
@@ -67,7 +67,7 @@ var cognitoTokenParam = function(email) {
 }
 
 exports.refresh = function(req,res) {
-    res.send(req.sessionID);
+    res.send(req.sessionID+'\n'+req.session.em+'\n'+req.session.cookie);
     /*
     if (req.session && req.session.em) {
         cognitoidentity.getOpenIdTokenForDeveloperIdentity(new cognitoTokenParam(req.session.em),function(err, data) { 
