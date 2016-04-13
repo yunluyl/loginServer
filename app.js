39e6a0eedd99a85e1881950db7608649d2866b2d
se7enWings <luyun198993@gmail.com>
2016-04-13 08:57:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+//libraries
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var DynamoDBStore = require('connect-dynamodb')({session: session});
+var AWS = require('aws-sdk');
+//local files
+var config = require('./config.js');
+var func = require('./func.js');
+
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+//configure express
+app.use(logger('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(session({
+                secret: 'kyocsf4',
+                saveUninitialized: false,
+                resave: false,
+                rolling: true,
+                cookie: {secure: true, maxAge: 604800},
+                store: new DynamoDBStore({
+                                        client: new AWS.DynamoDB(),
+                                        AWSRegion: 'us-east-1',
+                                        table: 'sessions',
+                                        reapInterval: 600000
+                        })
+            }));
+
+app.use(function(req, res, next) {
+    var err = req.session.error;
+    var msg = req.session.notice;
+    var success = req.session.success;
+
+    delete req.session.error;
+    delete req.session.notice;
+    delete req.session.notice;
+
+    if (err) res.locals.error = err;
+    if (msg) res.locals.notice = msg;
+    if (success) res.locals.success = success;
+
+    next();
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.post('/login',func.login);
+app.post('/refresh',func.refresh);
+app.put('/signup',func.signup);
+
+module.exports = app;
---
+++ func.js
@@ -0,0 +1,193 @@
+var AWS = require('aws-sdk');
+var bcrypt = require('bcryptjs');
+//Constants
+AWS.config.region = 'us-east-1';
+AWS.config.apiVersion = {
+    cognitoidentity: '2014-06-30',
+    dynamodb: '2012-08-10'
+};
+var errorDic = {
+    AWSGetItem:'AGI',
+    userNotExist:'UNE',
+    bcryptErr:'BCE',
+    wrongPassword:'WPW',
+    getTokenErr:'GTE',
+    userExist:'URE',
+    error7:'error7 occurred',
+    error8:'error8 occurred',
+    error9:'error9 occurred',
+    error10:'error10 occurred',
+};
+var cognitoidentity = new AWS.CognitoIdentity();
+var dynamodb = new AWS.DynamoDB();
+const saltRounds = 10;
+const authTableName = 'urs';
+
+//Object definition
+var putParam = function(email,passwordHash) {
+    this.TableName = authTableName;
+    this.Item = {
+        'em':{S:email},
+        'ph':{S:passwordHash}
+    };
+    this.ConditionExpression = 'attribute_not_exists(em)';
+}
+
+var getParam = function(email/*,ProjectionExpression1,ProjectionExpression2,...*/) {
+    this.TableName = authTableName;
+    this.Key = {
+        'em':{S:arguments[0]}
+    };
+    if (arguments.length > 1) {
+        this.ProjectionExpression = arguments[1];
+        for (var i=2;i<arguments.length;i++) {
+            this.ProjectionExpression += (','+arguments[i]);
+        }
+    }
+}
+
+var editParam = function(email,passwordHash,rstPass) {
+    this.TableName = authTableName;
+    this.Item = {
+        'em':{S:email},
+        'ph':{S:passwordHash},
+    };
+    if (rstPass) {
+        this.Item['rp'] = {BOOL:true};
+    }
+    this.ConditionExpression = 'attribute_exists(em)';
+}
+
+var cognitoTokenParam = function(email) {
+    this.IdentityPoolId = 'us-east-1:2ed0d413-562a-43db-b273-222585ff568d';
+    this.Logins = {
+        'login.test.developerLogin': email
+    };
+    this.TokenDuration = 3600;
+}
+
+exports.refresh = function(req,res) {
+    if (req.session && req.session.em) {
+        cognitoidentity.getOpenIdTokenForDeveloperIdentity(new cognitoTokenParam(req.session.em),function(err, data) { 
+            if (err) {
+                res.status(500).send({err: errorDic['getTokenErr']});
+            }
+            else {
+                res.status(200).send({AWSToken: data.Token});
+            }
+        });
+    }
+    else {
+        res.status(307).send({rdt: 'ULG'}); //redirect to user login
+    }
+}
+
+exports.login = function(req,res) {
+    dynamodb.getItem(new getParam(req.body.em),function(err,data) {
+        if (err) {
+            res.status(500).send({err: errorDic['AWSGetItem']});
+        }
+        else {
+            if (Object.keys(data).length !== 0) {
+                if (data.Item.hasOwnProperty('rp')) {
+                    res.status(307).send({rdt: 'RSP'}); //redirect to reset password
+                }
+                else {
+                    bcrypt.compare(req.body.pw,data.Item.ph.S,function(err,comResult) {
+                        if (err) {
+                            res.status(500).send({err: errorDic['bcryptErr']});
+                        }
+                        else {
+                            if (comResult) {
+                                cognitoidentity.getOpenIdTokenForDeveloperIdentity(new cognitoTokenParam(req.body.em),function(err, data) { if (err) {
+                                        res.status(500).send({err: errorDic['getTokenErr']});
+                                    }
+                                    else {
+                                        req.session.em = req.body.em;
+                                        res.status(200).send({AWSToken: data.Token});
+                                    }
+                                });
+                            }
+                            else {
+                                res.status(401).send({err: errorDic['wrongPassword']});
+                            }
+                        }
+                    });
+                }
+            }
+            else {
+                res.status(401).send({err: errorDic['userNotExist']});
+            }
+        }
+    });
+}
+
+exports.signup = function(req,res) {
+    bcrypt.hash(req.body.pw, saltRounds, function(err, hash) {
+        if (err) {
+            res.status(500).send({err: errorDic['bcryptErr']});
+        }
+        else {
+            dynamodb.putItem(new putParam(req.body.em, hash), function(err, data) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.status(200).send(); //signup sccessful
+                }
+            });
+        }
+    });
+}
+
+/*
+dynamodb.getItem(new getParam('b@b.com'),function(err,data) {
+    if (err) {
+        console.log('error!\n'+err);
+    }
+    else {
+        console.log(data);
+    }
+});
+*/
+
+/*
+dynamodb.putItem(new putParam('b@b.com','sdfsdsfsdfH79879'),function(err,data) {
+    if (err) {
+        console.log('error!\n'+err);
+    }
+    else {
+        console.log(data);
+    }
+});
+*/
+
+/*
+dynamodb.putItem(new editParam('c@c.com','sdfsdfsd5345',true),function(err,data) {
+    if (err) {
+        console.log('error!\n'+err);
+    }
+    else {
+        console.log(data);
+    }
+})
+*/
+
+
+
+
+
+
+/*
+var queryParam = function(email) {
+    this.TableName = authTableName;
+    this.IndexName = 'email-index';
+    this.KeyConditionExpression = 'email = :useremail';
+    this.ExpressionAttributeValues = {
+        ':useremail': {'S':email}
+    };
+    this.ExpressionAttributeNames = {'#c': 'Count'};
+    this.ProjectionExpression = '#c';
+}
+*/
+
