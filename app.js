aed1a648ecfd0e0abc8b178b7081f8b093ed8660
se7enWings <luyun198993@gmail.com>
2016-04-15 08:06:42 -0500

    test

--- app.js
+++ app.js
@@ -78,6 +78,6 @@ app.use(function(err, req, res, next) {
 app.post('/login',func.login);
 app.post('/refresh',func.refresh);
 app.put('/signup',func.signup);
-app.get('/activate',func.activate);
+app.post('/activate',func.activate);
 
 module.exports = app;
