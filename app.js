e54991481082de259f79cec2c4b475230eaed34b
se7enWings <luyun198993@gmail.com>
2016-04-14 21:24:54 -0500

    test

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.set('view engine', 'jade');
 
 //configure express
 app.use(logger('combined'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.set('trust proxy', 1);
 app.use(session({
